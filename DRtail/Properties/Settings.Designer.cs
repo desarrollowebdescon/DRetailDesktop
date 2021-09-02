a81dee87684e4988f6016397253f2a30e51f1ac9
dpaniaguaDes <daniel.paniagua@descon.mx>
2021-09-02 16:47:51 -0500

    Cambio Api

--- DRtail/Properties/Settings.Designer.cs
+++ DRtail/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace DRtail.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("http://54.39.26.9:62436/api/")]
+        [global::System.Configuration.DefaultSettingValueAttribute("http://64.250.121.122:2627/")]
         public string RutaApi {
             get {
                 return ((string)(this["RutaApi"]));
